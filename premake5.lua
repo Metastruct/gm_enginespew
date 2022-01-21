0faa13d04d474a7a0ae0fc5bbad9a85a82a1e451
Earu <earu.derg@gmail.com>
2022-01-21 13:07:21 +0100

    fix premake

--- premake5.lua
+++ premake5.lua
@@ -10,12 +10,10 @@ include(assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
 	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory"))
 
 CreateWorkspace({name = "enginespew"})
-	CreateProject({serverside = true})
-		includedirs({"src", "include"})
+	CreateProject({serverside = true, source_path = "src"})
 		IncludeSDKCommon()
 		IncludeSDKTier0()
 
-	CreateProject({serverside = false})
-		includedirs({"src", "include"})
+	CreateProject({serverside = false, source_path = "src"})
 		IncludeSDKCommon()
 		IncludeSDKTier0()
